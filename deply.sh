49d45e2357e888d162f92096a688876f706fba78
majdabbas <majd.abbas999@gmail.com>
2025-05-21 10:48:46 +0300

    init-commit

---
+++ deply.sh
@@ -0,0 +1,18 @@
+# Check if ollama is installed
+if command -v ollama &> /dev/null
+then
+    echo "✅ Ollama is already installed."
+else
+    echo "⬇️ Ollama not found. Installing now..."
+    curl -fsSL https://ollama.com/install.sh | sh
+
+    # Recheck if installation succeeded
+    if command -v ollama &> /dev/null
+    then
+        echo "✅ Ollama installed successfully."
+    else
+        echo "❌ Ollama installation failed."
+        exit 1
+    fi
+fi
+ollama pull gemma3:1b
